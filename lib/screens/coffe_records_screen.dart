18fa299e72b47b6aa4dcabb845d623645124fde5
Atia Ibnat Mouni <ibnat567atia@gmail.com>
2026-08-10 03:33:04 +0600

    Fix coffee records Provider usage

--- lib/screens/coffe_records_screen.dart
+++ lib/screens/coffe_records_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:summer_iub_app/models/coffee_records_model.dart';
 import 'package:summer_iub_app/state_management/coffee_state_management.dart';
-
 import 'package:summer_iub_app/widgets/app_backgroud_design_widget.dart';
 
 class CoffeRecordsScreen extends StatefulWidget {
@@ -13,32 +12,11 @@ class CoffeRecordsScreen extends StatefulWidget {
 }
 
 class _CoffeRecordsScreenState extends State<CoffeRecordsScreen> {
-  late Provider<CoffeeStateManagement> csm;
-
-  @override
-  initState() {
-    super.initState();
-    csm =
-        Provider.of<CoffeeStateManagement>(context, listen: false)
-            as Provider<CoffeeStateManagement>;
-  }
-
-  @override
-  void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
-    super.didChangeDependencies();
-  }
-
-  @override
-  dispose() {
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Coffee Records",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.00),
         ),
@@ -50,17 +28,19 @@ class _CoffeRecordsScreenState extends State<CoffeRecordsScreen> {
         builder: (context, csm, _) {
           return AppBackgroudDesignWidget(
             child: ListView.builder(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
               itemCount: csm.items.length,
               itemBuilder: (context, index) {
                 final CoffeeRecordsModel coffeeRecord = csm.items[index];
 
                 return Card(
                   child: ListTile(
-                    leading: Icon(Icons.coffee),
+                    leading: const Icon(Icons.coffee),
                     title: Text(coffeeRecord.title ?? ''),
                     subtitle: Text(
-                      "${coffeeRecord.des} - Amount: ${coffeeRecord.amount} - ID: (${coffeeRecord.id})",
+                      "${coffeeRecord.des ?? ''} - "
+                      "Amount: ${coffeeRecord.amount ?? 0} - "
+                      "ID: (${coffeeRecord.id})",
                     ),
                   ),
                 );
@@ -75,9 +55,8 @@ class _CoffeRecordsScreenState extends State<CoffeRecordsScreen> {
           return FloatingActionButton(
             onPressed: () {
               csm.addData();
-              setState(() {});
             },
-            child: Icon(Icons.local_cafe),
+            child: const Icon(Icons.local_cafe),
           );
         },
       ),
